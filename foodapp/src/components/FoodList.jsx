545df16c6f59b2b120631bcdd82164e2eaec9d9a
kasulaarunteja <kasulaarunteja@gmail.com>
2022-08-13 15:57:47 +0530

    done

---
+++ foodapp/src/components/FoodList.jsx
@@ -0,0 +1,62 @@
+
+
+
+import axios from "axios";
+import  React from "react";
+import { useState, useEffect } from "react";
+import Header from "./Header";
+import { useNavigate } from 'react-router';
+
+
+
+export default function FoodList() {
+
+    const [data, setData] = useState([]);
+
+    const [loding, setLoding] = useState(true);
+
+
+    const navigate= useNavigate();
+
+    useEffect(()=>{
+        axios.get("http://localhost:8080/food").then((res) => {
+            setLoding(false);
+             setData(res.data);
+          
+        })
+    }, [])
+
+      console.log(data)
+          
+
+    return loding ? "loding" :  (
+        
+        <div>
+           <Header/>
+           <div className="container">
+            <div className="left">FoodList</div>
+            <div className="right">Favourites</div>
+           </div>
+           <div className="Details">
+            {data.map((ele) => {
+                return(
+                    <div onClick={() => navigate(`/${ele.code}`)}
+                    className="pro-details" 
+                    key={ele.code}>
+    
+                        <img src="img/icon.png" alt="NOT FOUND" className="icon" />
+                            <div className="indiv-pro">
+                                <h5>
+                                    {ele.product_name} ({ele.generic_name})
+                                </h5>
+                            </div>
+                    </div>
+                )
+                
+            })}
+           </div>
+        </div>
+    )
+
+
+}
---
+++ foodapp/src/components/Single.jsx
@@ -0,0 +1,65 @@
+
+
+
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import Header from "./Header";
+
+
+export default function  Single(){
+
+    const {code} = useParams();
+
+    const[product, setProduct] = useState({});
+
+    useEffect(() => {
+        getData();
+        
+    }, []);
+
+     const getData = () =>{
+        axios.get(`http://localhost:8080/food?code=${code}`).then((res) => {
+            let findcode = res.data.filter((el) => el.code == code)
+            //console.log(findcode)
+            setProduct(findcode[0]);
+           // setProduct(res.data[0])
+        })
+      
+    }
+
+    // const handleChange =(data)=>{
+    //     axios.post("http://localhost:8080/fav", data).then(("data"))
+        
+    // }
+
+    return (
+        <div>
+            <Header />
+            <div className="details">
+    <div className="pro-div">
+        <img src="img/icon.png" alt="not found" className="Icon-img-src" />
+        <h4 className="Pro-tag">{product.product_name}</h4>
+        <button variant="contained" onClick={()=>{}}> +Favourities</button>
+    </div>
+
+    <div className="table-main-container">
+        <table>
+            <tbody>
+                {Object.keys(product).map((key, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{key}</td>
+                            <td>{product[key]}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+       </div>
+     </div>
+
+        </div>
+    )
+}
