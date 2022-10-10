37a094f3f7c99e848c069fb5d2e4c476cec4489b
kasulaarunteja <kasulaarunteja@gmail.com>
2022-10-10 20:16:56 +0530

    head

--- foodapp/src/components/Header.jsx
+++ foodapp/src/components/Header.jsx
@@ -1,18 +1,11 @@
-
-
 import React from "react";
 import { useNavigate } from "react-router";
 
-
 export default function Header() {
-	const navigate = useNavigate()
-	return (
-		<div
-			className="head-container"
-			onClick={() => navigate("/")}
-		>
-			<h2 className="head-title">TheFoodList</h2>
-			
-		</div>
-	);
-};
+  const navigate = useNavigate();
+  return (
+    <div className="head-container" onClick={() => navigate("/")}>
+      <h2 className="head-title">TheFoodList</h2>
+    </div>
+  );
+}
