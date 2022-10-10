1af1bb4c5d75830a58bdb89c0ced6c1346314567
kasulaarunteja <kasulaarunteja@gmail.com>
2022-10-10 20:17:12 +0530

    sing

--- foodapp/src/components/Single.jsx
+++ foodapp/src/components/Single.jsx
@@ -1,69 +1,62 @@
-
-
-
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Header from "./Header";
 
+export default function Single() {
+  const { code } = useParams();
 
-export default function  Single(){
+  const [product, setProduct] = useState({});
 
-    const {code} = useParams();
+  useEffect(() => {
+    getData();
+  }, []);
 
-    const[product, setProduct] = useState({});
+  const getData = () => {
+    axios
+      .get(`https://invation101.herokuapp.com/food?code=${code}`)
+      .then((res) => {
+        let findcode = res.data.filter((el) => el.code == code);
+        //console.log(findcode)
+        setProduct(findcode[0]);
+        // setProduct(res.data[0])
+      });
+  };
 
-    useEffect(() => {
-        getData();
-        
-    }, []);
+  // const handleChange =(data)=>{
+  //     axios.post("http://localhost:8080/fav", data).then(("data"))
 
-     const getData = () =>{
-        axios.get(`https://invation101.herokuapp.com/food?code=${code}`).then((res) => {
-            let findcode = res.data.filter((el) => el.code == code)
-            //console.log(findcode)
-            setProduct(findcode[0]);
-           // setProduct(res.data[0])
-        })
-      
-    }
+  // }
 
-    // const handleChange =(data)=>{
-    //     axios.post("http://localhost:8080/fav", data).then(("data"))
-        
-    // }
-
-    return (
-        <div>
-            <Header />
-            <div className="container">
-            <div className="left">FoodList</div>
-            <div className="right">Favourites</div>
-           </div>
-            <div className="details">
-    <div className="pro-div">
-        <img src="img/icon.png" alt="not found" className="Icon-img-src" />
-        <h4 className="Pro-tag">{product.product_name}</h4>
-      
-    </div>
-
-    <div className="table-main-container">
-        <table>
-            <tbody>
-                {Object.keys(product).map((key, index) => {
-                    return (
-                        <tr key={index}>
-                            <td>{key}</td>
-                            <td>{product[key]}</td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
-       </div>
-     </div>
-
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <div className="left">FoodList</div>
+        <div className="right">Favourites</div>
+      </div>
+      <div className="details">
+        <div className="pro-div">
+          <img src="img/icon.png" alt="not found" className="Icon-img-src" />
+          <h4 className="Pro-tag">{product.product_name}</h4>
         </div>
-    )
+
+        <div className="table-main-container">
+          <table>
+            <tbody>
+              {Object.keys(product).map((key, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{key}</td>
+                    <td>{product[key]}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
